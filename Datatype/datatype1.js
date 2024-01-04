87a79f8738120857d70f227a0ccbb2eff6d1e819
gautammanak1 <gautammanak1@gmail.com>
2024-01-04 12:31:10 +0530

    DataType1

---
+++ Datatype/datatype1.js
@@ -0,0 +1,21 @@
+// Datatypes: Node.js contains various types of data types similar to JavaScript.
+//Boolean,Undefined,Null,String,Number
+//Loose Typing: Node.js supports loose typing, which means you don’t need to specify what type of information will be stored in a variable in advance. We use the var and let keywords in Node.js declare any type of variable. Examples are given below:
+
+//Example:
+
+// Variable store number data type
+let a = 35;
+console.log(typeof a);
+
+// Variable store string data type
+a = "Gautam Manak";
+console.log(typeof a);
+
+// Variable store Boolean data type
+a = true;
+console.log(typeof a);
+
+// Variable store undefined (no value) data type
+a = undefined;
+console.log(typeof a);
