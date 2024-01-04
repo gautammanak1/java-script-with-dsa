91b19dae2d5ad7fe1ff250112a8522be27289313
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2024-01-04 12:43:39 +0530

    Update datatype2.js
    data type2

---
+++ Datatype/datatype2.js
@@ -0,0 +1,14 @@
+//  Objects & Functions: Node.js objects are the same as JavaScript objects i.e. the objects are similar to variables and it contains many values which are written as name: value pairs. Name and value are separated by a colon and every pair is separated by a comma.
+
+let company = {
+  Name: "MeerutCodeHub",
+  Address: "Meerut",
+  Contact: "+919997355153",
+  Email: "meerutcodehub@gmail.com",
+};
+
+// Display the object information
+console.log("Information of variable company:", company);
+
+// Display the type of variable
+console.log("Type of variable company:", typeof company);
