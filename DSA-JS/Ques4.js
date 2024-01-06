86c6cea7e894ec03295abbdcf65306534d37fc23
gautammanak1 <gautammanak1@gmail.com>
2024-01-06 14:51:57 +0530

    Return an array showing the cumulative sum at each index of an array of integers

--- DSA-JS/Ques4.js
+++ DSA-JS/Ques4.js
@@ -2,12 +2,11 @@
 
 let arr = [1, 9, 5, 1];
 
-const cumulativeSum = list => {
-    let result = [list[0]];
-    for (let i = 1; i <  list.length; i++) {
-        result.push(list[i] + result[i - 1]);
-
-    }
-    return result;
-} 
+const cumulativeSum = (list) => {
+  let result = [list[0]];
+  for (let i = 1; i < list.length; i++) {
+    result.push(list[i] + result[i - 1]);
+  }
+  return result;
+};
 console.log(cumulativeSum(arr));
