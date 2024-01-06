8303fd2d193bf5ab363740d4391f3976c2ab5c39
gautammanak1 <gautammanak1@gmail.com>
2024-01-06 14:18:11 +0530

    Find the missing number in a given integer array of 1 to 100

--- DSA-JS/Ques1.js
+++ DSA-JS/Ques1.js
@@ -1,5 +1,5 @@
 //Find the missing number in a given integer array of 1 to 100
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+let arr = [1, 2, 3, 4, 5, 7, 8, 9, 10];
 const findMisssingNum = (arr) => {
   for (var i = 0; i < arr.length - 1; i++) {
     if (arr[i] + 1 != arr[i + 1]) {
