aa4c52cd14710151f1e2c6391dee4b6afbdab51e
gautammanak1 <gautammanak1@gmail.com>
2024-01-06 14:47:22 +0530

    Find a duplicate number in an array of integers

--- DSA-JS/Ques2.js
+++ DSA-JS/Ques2.js
@@ -1,17 +1,15 @@
 //Find a duplicate number in an array of integers
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 4, 10];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 5, 10];
 const findDupes = (arr) => {
-    const observed = {};
-    for (let i = 0; i < arr.length; i++){
-        if (observed[arr[i]]) {
-            return arr[i]
-        }
-        else {
-            observed[arr[i]] = arr[i];
-        }
-            
+  const observed = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (observed[arr[i]]) {
+      return arr[i];
+    } else {
+      observed[arr[i]] = arr[i];
     }
-    return false;
-}
+  }
+  return false;
+};
 console.log(findDupes(arr));
