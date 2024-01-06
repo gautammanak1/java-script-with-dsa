6cfc9bcdb98eaaafcf2e7ec52967ab217132a951
gautammanak1 <gautammanak1@gmail.com>
2024-01-06 14:03:46 +0530

    Reverse a String

---
+++ DSA-JS/9.js
@@ -0,0 +1,7 @@
+// Reverse a string
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+//Lets Check
+let text = "I am Gautam Manak I am Full Stack Developer(MERN) at KloudiDev";
+console.log(reverseString(text));
