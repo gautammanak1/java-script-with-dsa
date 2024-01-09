eb704a5661e2484a31560cfadf529796f8d11f58
gautammanak1 <gautammanak1@gmail.com>
2024-01-09 15:42:56 +0530

    Implement a linked list.

---
+++ DSA-JS/21.js
@@ -0,0 +1,32 @@
+//Implement a linked list.
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(data) {
+    let newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+
+// Example usage:
+let list = new LinkedList();
+list.insert(1);
+list.insert(2);
+list.insert(3);
