bf603a1c5d7305c8d27716619626ede7391ede3f
gautammanak1 <gautammanak1@gmail.com>
2024-01-10 11:39:16 +0530

    Find the longest substring without repeating characters.

---
+++ DSA-JS/24.js
@@ -0,0 +1,20 @@
+// Find the longest substring without repeating characters.
+function gautamManak(str) {
+  let longest = 0;
+  let start = 0;
+  let charMap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+    if (charMap[currentChar] >= start) {
+      start = charMap[currentChar] + 1;
+    }
+    charMap[currentChar] = i;
+    longest = Math.max(longest, i - start + 1);
+  }
+
+  return longest;
+}
+//Lets Check
+let text = "abcabcbb";
+console.log(gautamManak(text));
