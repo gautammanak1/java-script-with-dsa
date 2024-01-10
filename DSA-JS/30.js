c490eb3e83dbb67bf55348d37179802fbcee6d42
gautammanak1 <gautammanak1@gmail.com>
2024-01-10 12:53:07 +0530

    Implement the merge sort algorithm.

---
+++ DSA-JS/30.js
@@ -0,0 +1,34 @@
+//Implement the merge sort algorithm.
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+// Lets Check
+let unsortedArray = [5, 3, 8, 4, 2, 7, 1];
+console.log(mergeSort(unsortedArray));
